1a04e823ec7ccf86550a0f91bb37cb0c6d73f7fe
dsadasdas dsadsadas <mike13.lv@gmail.com>
2020-11-12 20:51:30 +0200

    added exception classes

---
+++ exceptions.py
@@ -0,0 +1,8 @@
+class BrowserError(Exception):
+    "Raise in case of issue with browser action"
+
+class NoSuchAttributeError(Exception):
+    "Raise in case of attribute can not be found on the element"
+
+class NoSuchElementError(Exception):
+    "Raise in case of element can not be found"
